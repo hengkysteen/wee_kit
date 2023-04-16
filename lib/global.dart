7ea04748c07fdd212e4ca2ff92fc7c2dc23fdcca
Hengky Steen <hengkysteen@gmail.com>
2023-04-16 19:11:10 +0700

    rename to WeeGlobalNavState

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class WeeGlobal {
+class WeeGlobalNavState {
   static GlobalKey<NavigatorState> key = GlobalKey<NavigatorState>();
   static BuildContext? get context => key.currentContext;
   static NavigatorState? get state => key.currentState;

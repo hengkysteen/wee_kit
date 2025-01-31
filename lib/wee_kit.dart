829a4f17d75ecaaae51a692ee874463e35dfc776
Hengky Steen <hengkysteen@gmail.com>
2025-01-31 08:40:36 +0700

    export dot_loading

--- lib/wee_kit.dart
+++ lib/wee_kit.dart
@@ -5,10 +5,13 @@ export 'src/widgets/show.dart';
 export 'src/widgets/loadmore.dart';
 export 'src/widgets/input_format.dart';
 export 'src/widgets/divider_centered_text.dart';
-export 'src/utils/debouncer.dart';
-export 'src/utils/datetime.dart';
-export 'src/widgets/color.dart';
-export 'src/extentions/index.dart';
-export 'package:intl/intl.dart';
 export 'src/widgets/animate/animated.dart';
 export 'src/widgets/animate/transitions/index.dart';
+export 'src/widgets/color.dart';
+export 'src/widgets/dot_loading.dart';
+
+export 'src/utils/debouncer.dart';
+export 'src/utils/datetime.dart';
+
+export 'src/extentions/index.dart';
+export 'package:intl/intl.dart';
